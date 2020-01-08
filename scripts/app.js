852e7014f665c4f2c9f60ee52ec90e60d1cd62d4
JohnSM <john.stringer.martin@gmail.com>
2020-01-08 19:24:03 +0000

    added gender predict into div

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,9 @@ const cameraView = document.getElementById('cameraview')
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.ageGenderNet.loadFromUri('/models')
-]).then(() => {
+]).then(startVideo)
+
+function startVideo() {
   navigator.getUserMedia(
     {
       video: {}
@@ -11,30 +13,39 @@ Promise.all([
     stream => cameraView.srcObject = stream,
     err => console.warn(err)
   )
-});
+}
 
+// document.getElementById("myText").value = `${faceapi.round(age, 0)} years`,
+// `${gender} (${faceapi.round(genderProbability)})`;
 
 cameraView.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(cameraView)
   document.body.append(canvas)
   const size = { width: cameraView.width, height: cameraView.height }
   faceapi.matchDimensions(canvas, size)
+  let genderField = document.getElementById("genderField");
+  let probField = document.getElementById("probField");
  
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(cameraView, new faceapi.TinyFaceDetectorOptions())
       .withAgeAndGender();
 
+      console.log(detections);
+
+      
+
+
     const resizedResults = faceapi.resizeResults(detections, size);
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
     resizedResults.forEach(result => {
       const { age, gender, genderProbability } = result
-      new faceapi.draw.DrawTextField(
-        [
-          `${faceapi.round(age, 0)} years`,
-          `${gender} (${faceapi.round(genderProbability)})`
-        ],
-        result.detection.box.bottomRight
-      ).draw(canvas);
+
+      genderField.innerHTML = "";
+      genderField.innerHTML += `${gender}`;
+
+      probField.innerHTML = "";
+      probField.innerHTML += `${faceapi.round(genderProbability*100)}`;
+        
     });
-  }, 100);
+
+  }, 2000);
 });
